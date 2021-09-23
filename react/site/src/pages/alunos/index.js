bdb87d9bfb7ca3665e3bb731196582dad20b2e62
John Vlogs <ra48430556893@acaonsfatima.org.br>
2021-09-23 17:26:47 -0300

    ok

--- react/site/src/pages/alunos/index.js
+++ react/site/src/pages/alunos/index.js
@@ -35,7 +35,7 @@ export default function Index() {
 
     async function inserir() {
 
-        if(idAlterando == 0) {
+        if(idAlterando === 0) {
             let r = await api.inserir(nome, chamada, curso, turma);
             if (r.erro) 
                 toast.error(r.erro);
@@ -118,7 +118,7 @@ export default function Index() {
                         
                         <div class="text-new-student">
                             <div class="bar-new-student"></div>
-                            <div class="text-new-student"> {idAlterando == 0 ? "Novo Aluno" : "Alterando Aluno" + idAlterando} </div>
+                            <div class="text-new-student"> {idAlterando === 0 ? "Novo Aluno" : "Alterando Aluno" + idAlterando} </div>
                         </div>
 
                         <div class="input-new-student"> 
@@ -143,7 +143,7 @@ export default function Index() {
                                     <div class="input"> <input type="text" value={turma} onChange={e => setTurma(e.target.value)} /> </div> 
                                 </div>
                             </div>
-                            <div class="button-create"> <button onClick={inserir}> {idAlterando == 0 ? "cadastrar" : "Alterar"} </button> </div>
+                            <div class="button-create"> <button onClick={inserir}> {idAlterando === 0 ? "cadastrar" : "Alterar"} </button> </div>
                         </div>
                     </div>
 
@@ -170,7 +170,7 @@ export default function Index() {
 
                                {alunos.map((item, i) =>
 
-                                <tr className={i % 2 ==0 ?"linha-alternada" : ""}>
+                                <tr className={i % 2 ===0 ?"linha-alternada" : ""}>
                                     <td> {item.id_matricula} </td>
                                     <td title={item.nm_aluno}> {item.nm_aluno != null&& item.nm_aluno.length >= 25 ? item.nm_aluno.substr(0, 25) + '...' : item.nm_aluno } </td>
                                     <td> {item.nr_chamada} </td>
